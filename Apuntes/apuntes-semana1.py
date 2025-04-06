43c371b07a011b48d86018e8bc4122bdad08aaa0
topias1903 <tobias_alamedas@hotmail.com>
2025-04-06 16:21:36 -0300

    Ramas

---
+++ Apuntes/apuntes-semana1.py
@@ -0,0 +1,24 @@
+#OPERADORES ARITMETICOS
+
+#PRINT E INPUT
+
+nombre = input("Ingresa tu nombre: ")
+print("Hola,", nombre)
+
+#Numeros enteros
+
+numero = input("Ingresa un numero: ")
+numero = int(numero)
+
+print("El numero ingresado es:", numero)
+
+#Numero decimal
+
+numero_decimal = input("Ingresa un numero decimal: ")
+numero_decimal = float(numero_decimal)
+
+print ("El numero decimal ingresado es:", numero_decimal)
+
+#Formato string
+
+print(f"Tu nombre es {nombre} y el numero entero ingresado es {numero}")
